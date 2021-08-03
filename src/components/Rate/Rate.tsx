261aa5cdb3d5cd521b01742485eeaf73e50278c2
aleksandreleskin <eleskin1201@gmail.com>
2021-08-03 19:07:44 +0300

    add rate chart

--- src/components/Rate/Rate.tsx
+++ src/components/Rate/Rate.tsx
@@ -1,4 +1,5 @@
 import styles from './Rate.module.css';
+import ReactApexChart from 'react-apexcharts';
 
 const Rate = () => {
   return (
@@ -10,6 +11,72 @@ const Rate = () => {
           <span><span>----</span> DAI</span>
         </div>
       </header>
+
+      <ReactApexChart
+        options={{
+          chart: {
+            type: 'area',
+            toolbar: {
+              show: false,
+            },
+            sparkline: {
+              enabled: true
+            }
+          },
+          dataLabels: {
+            enabled: false
+          },
+          fill: {
+            colors: ['#39F1C2', '#99F8DF', '#ffffff']
+          },
+          stroke: {
+            curve: 'smooth',
+            width: 2,
+            colors: ['#00CD98']
+          },
+          xaxis: {
+            labels: {
+              show: false
+            },
+            axisBorder: {
+              show: false,
+            },
+            axisTicks: {
+              show: false,
+            },
+          },
+          yaxis: {
+            labels: {
+              show: false
+            },
+            axisBorder: {
+              show: false,
+            },
+            axisTicks: {
+              show: false,
+            },
+          },
+          tooltip: {
+            enabled: false
+          },
+          grid: {
+            show: false,
+            padding: {
+              left: 0,
+              right: 0,
+              bottom: 0
+            }
+          }
+        }}
+        series={[
+          {
+            name: 'series1',
+            data: [31, 40, 28, 51, 42, 109, 100]
+          },
+        ]}
+        type="area"
+        height={180}
+      />
     </div>
   );
 };
